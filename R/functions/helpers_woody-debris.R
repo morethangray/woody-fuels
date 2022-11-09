8701a719447d7a1fc5ff75ee970db6e2a74fae3d
morethangray <morethangray@users.noreply.github.com>
2022-11-09 14:45:54 -0800

    qc markdown for tubbs (dl)

---
+++ R/functions/helpers_woody-debris.R
@@ -0,0 +1,19 @@
+# Define helpers  ----
+index_dl <- "Duff & litter"
+index_dl_si <- "Mean fuel depth (centimeters)"
+index_dl_si_abbr <- "cm"
+index_wd <- "Coarse woody debris"
+index_wd_si <- "Mean fuel load (metric tons per hectare)"
+index_wd_si_abbr <- "MT/ha"
+# list_classes_wd <- c("all", "hr0001", "hr0010", "hr0100", "hr1000r", "hr1000s")
+
+# Define plot colors ----
+colors_year <- c("gray70", "#C47E61", "#dbae84", "#e9d097")
+# Subset palette to colors for 2017, 2021
+colors_year_subset <- c(colors_year[2], colors_year[4])
+colors_dl <- c("#7da8b0", "#c4c4c4")
+colors_wd <- c("#7da8b0", "#c4c4c4")
+colors_wd5 <- c("gray25", "#5c5c5c", "#858585", "gray85","#adadab")
+
+colors_thin_bright <- c("#9d9596", "#069879")
+colors_thin_faded <- c("#bfbabb", "#75bca8")
